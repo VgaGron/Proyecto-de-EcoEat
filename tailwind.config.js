4f2ff63a5cc88c368b0917d1a13512b633e6e643
Dilmer <vegagiron2005@gmai.com>
2026-07-09 17:58:50 -0500

    fix: actualizar content de tailwind para incluir src/

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}", 
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
